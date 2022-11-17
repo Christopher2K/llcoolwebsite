b9f15513938e012a8b33594b3f555f4426f2776c
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-11-16 19:12:29 -0500

    chore: install svelte pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import node from '@astrojs/node'
+import svelte from '@astrojs/svelte'
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
+  integrations: [svelte()],
 })
