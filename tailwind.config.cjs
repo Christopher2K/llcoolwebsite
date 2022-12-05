fe2b80a5b247c68547edb6305642b944b83aaf48
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-12-04 22:36:20 -0500

    feat: footer component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,39 +7,40 @@ module.exports = {
       body: ['Lato', 'sans-serif'],
     },
 
-    colors: {
-      'background-light': '#f3f4f6',
-      'background-dark': '#242424',
-
-      primary: {
-        DEFAULT: '#99a8ff',
-        50: '#cbdaff',
-        100: '#c1d0ff',
-        200: '#b7c6ff',
-        300: '#adbcff',
-        400: '#a3b2ff',
-        500: '#99a8ff',
-        600: '#8f9ef5',
-        700: '#8594eb',
-        800: '#7b8ae1',
-        900: '#7180d7',
-      },
-
-      secondary: {
-        DEFAULT: '#ed8e15',
-        50: '#ffc047',
-        100: '#ffb63d',
-        200: '#ffac33',
-        300: '#ffa229',
-        400: '#f7981f',
-        500: '#ed8e15',
-        600: '#e3840b',
-        700: '#d97a01',
-        800: '#cf7000',
-        900: '#c56600',
-      },
-    },
     extend: {
+      colors: {
+        light: '#f3f4f6',
+        dark: '#242424',
+
+        primary: {
+          DEFAULT: '#99a8ff',
+          50: '#cbdaff',
+          100: '#c1d0ff',
+          200: '#b7c6ff',
+          300: '#adbcff',
+          400: '#a3b2ff',
+          500: '#99a8ff',
+          600: '#8f9ef5',
+          700: '#8594eb',
+          800: '#7b8ae1',
+          900: '#7180d7',
+        },
+
+        secondary: {
+          DEFAULT: '#ed8e15',
+          50: '#ffc047',
+          100: '#ffb63d',
+          200: '#ffac33',
+          300: '#ffa229',
+          400: '#f7981f',
+          500: '#ed8e15',
+          600: '#e3840b',
+          700: '#d97a01',
+          800: '#cf7000',
+          900: '#c56600',
+        },
+      },
+
       gridTemplateRows: {
         mobile: '72px minmax(0, 1fr)',
       },
