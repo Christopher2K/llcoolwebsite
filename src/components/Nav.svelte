0c1a2d09d9e69d82509e38111f3254ccc580e9f1
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-12-24 01:21:28 +0100

    feat: nav design fixed

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,6 +5,7 @@
   import Typography from './Typography.svelte'
   import RoundedIconButton from './RoundedIconButton.svelte'
   import NavItem from './NavItem.svelte'
+  import Socials from './Socials.svelte'
 
   // Props
   export let path: string
@@ -43,46 +44,66 @@
 <nav
   class="w-full sticky top-0 left-0 p-4 h-fit md:relative md:flex md:flex-col md:justify-start md:items-center md:gap-10"
 >
-  <div class="h-[72px] flex flex-row justify-between items-center">
+  <div class="h-[72px] md:h-auto lg:w-full flex flex-row justify-between items-center">
     <Typography variant="subheading2" class="md:hidden">
       <a href="/">Christopher</a>
     </Typography>
 
-    <div class="flex flex-row justify-start items-center gap-4">
-      <RoundedIconButton
-        class="md:hidden"
-        label={menuButtonLabel}
-        type="button"
-        on:click={toggleMenu}
-      >
-        <svelte:component
-          this={menuButtonIcon}
-          class="h-6 text-neutral-50"
-          aria-hidden="true"
-        />
-      </RoundedIconButton>
+    <div
+      class="flex flex-col lg:flex-row-reverse justify-start lg:justify-between items-center md:gap-10 lg:gap-6 lg:w-full"
+    >
+      <div class="flex flex-row justify-start items-center gap-4">
+        <RoundedIconButton
+          class="md:hidden"
+          label={menuButtonLabel}
+          type="button"
+          on:click={toggleMenu}
+        >
+          <svelte:component
+            this={menuButtonIcon}
+            class="h-6 text-neutral-50"
+            aria-hidden="true"
+          />
+        </RoundedIconButton>
 
-      <RoundedIconButton label={themeButtonLabel} type="button">
-        <svelte:component
-          this={themeButtonIcon}
-          class="h-6 text-neutral-50"
-          aria-hidden="true"
-        />
-      </RoundedIconButton>
+        <RoundedIconButton label={themeButtonLabel} type="button">
+          <svelte:component
+            this={themeButtonIcon}
+            class="h-6 text-neutral-50"
+            aria-hidden="true"
+          />
+        </RoundedIconButton>
+      </div>
+
+      <img
+        class="hidden md:block rounded-full h-16 w-16"
+        src="/images/chris.jpg"
+        alt="Christopher smiling"
+      />
     </div>
   </div>
 
-  <img
-    class="hidden md:block rounded-full h-16 w-16"
-    src="/images/chris.jpg"
-    alt="Christopher smiling"
-  />
+  <section class="hidden self-start lg:flex flex-col gap-4">
+    <div>
+      <Typography>Christopher N. Katoyi Kaba</Typography>
+      <Typography variant="hint2">@LLCoolChris_</Typography>
+    </div>
+    <div>
+      <Typography>Software Engineer @ Maple</Typography>
+      <Typography>Educational Tech Stream</Typography>
+    </div>
+    <Typography variant="hint2">Toronto, Canada</Typography>
+  </section>
+
+  <section class="hidden self-start lg:flex flex-row justify-start items-start gap-4">
+    <Socials />
+  </section>
 
   <!-- Nav menu -->
   <div
-    class="md:flex absolute md:relative flex-col justify-start items-start gap-5 bg-light px-4 md:px-0 top-full md:top-auto left-0 md:left-auto right-0 md:right-auto h-[calc(100vh-72px-theme(space.4)-theme(space.4))] md:h-auto w-full"
     class:hidden={!menuOpen}
     class:flex={menuOpen}
+    class="md:flex absolute md:relative flex-col justify-start items-start gap-5 bg-light px-4 md:px-0 top-full md:top-auto left-0 md:left-auto right-0 md:right-auto h-[calc(100vh-72px-theme(space.4)-theme(space.4))] md:h-auto w-full"
   >
     <NavItem hideTextOnTablet label="Home" href="/" currentPath={path} absolute>
       <Home slot="icon" />
---
+++ src/components/Socials.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Twitch, Twitter, Github, Linkedin } from 'lucide-svelte'
+
+  const SOCIALS = [
+    { name: 'Twitch', href: 'https://twitch.tv/LLCoolChris_', component: Twitch },
+    { name: 'Twitter', href: 'https://twitter.com/LLCoolChris_', component: Twitter },
+    { name: 'Github', href: 'https://github.com/Christopher2k', component: Github },
+    {
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/in/christopherkatoyi/',
+      component: Linkedin,
+    },
+  ]
+</script>
+
+<section>
+  <ul class="flex flex-row justify-start items-center gap-4">
+    {#each SOCIALS as social}
+      <li>
+        <a href={social.href} target="_blank" rel="noreferrer" aria-label={social.name}>
+          <svelte:component this={social.component} class="h-6 w-6" aria-hidden="true" />
+        </a>
+      </li>
+    {/each}
+  </ul>
+</section>
