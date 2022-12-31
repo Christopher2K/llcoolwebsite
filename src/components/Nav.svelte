069f9dbeefa964a41efe376d8316bffca5f8f4d1
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-12-31 15:23:38 +0100

    fix: z-indexes

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -54,7 +54,7 @@
 </script>
 
 <nav
-  class="w-full sticky top-0 left-0 p-4 h-fit md:flex md:flex-col md:justify-start md:items-center md:gap-10 bg-light dark:bg-dark"
+  class="w-full sticky top-0 left-0 p-4 h-fit md:flex md:flex-col md:justify-start md:items-center md:gap-10 bg-light dark:bg-dark z-40"
 >
   <div class="h-[72px] md:h-auto lg:w-full flex flex-row justify-between items-center">
     <Typography variant="subheading2" class="md:hidden">
