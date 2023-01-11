035ef4fd5b16ed20935e87df9b8c2fb79ef23cbf
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-01-10 22:00:03 -0500

    fix: tweak localize fn to have something more reliable

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
+import config from '../astro-i18next.config'
+
 export function localizePath(path: string, lang: string) {
-  if (lang === 'en') return path
-  return `/${lang}${path}`
+  const [maybeLocale, ...rest] = path.slice(1).split('/')
+  let fragment = path
+  if (maybeLocale && config.locales.includes(maybeLocale)) {
+    fragment = `/${rest.join('/')}`
+  }
+
+  if (lang === 'en') return fragment
+  return `/${lang}${fragment}`
 }
