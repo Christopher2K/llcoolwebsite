f3b5812cd788161c6e22b862f8c0c84f8bc5d58a
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-03-05 16:57:46 -0500

    fix: missing translation for blog page

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,7 +3,7 @@ import type { AstroI18nextConfig } from 'astro-i18next/index'
 const config: AstroI18nextConfig = {
   defaultLocale: 'en',
   locales: ['en', 'fr'],
-  namespaces: ['common', 'home', 'footer', 'indieStories', 'about'],
+  namespaces: ['common', 'home', 'footer', 'indieStories', 'about', 'blog'],
   load: ['server'],
   i18nextClient: {
     detection: {
